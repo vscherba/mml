f381b5f72645d6a6853ff2d8d2e4bdcdb4676b04
Victor Scherba <vic.scherba@gmail.com>
2013-12-26 23:55:43 +0400

    Refactoring. Move target functions holder to most derived class to avoid constructor forwarding.

--- mml/inline_overloaded_fn/inline_overloaded_fn.hpp
+++ mml/inline_overloaded_fn/inline_overloaded_fn.hpp
@@ -14,15 +14,12 @@
 // boost::preprocessor
 #include <boost/preprocessor/repetition/enum.hpp>
 #include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/repetition/enum_trailing_params.hpp>
 #include <boost/preprocessor/repetition/enum_binary_params.hpp>
 #include <boost/preprocessor/repetition/repeat.hpp>
 #include <boost/preprocessor/facilities/intercept.hpp>
 #include <boost/preprocessor/arithmetic/inc.hpp>
 
 // boost::mpl
-#include <boost/mpl/long.hpp>
-#include <boost/mpl/if.hpp>
 #include <boost/mpl/at.hpp>
 #include <boost/mpl/front.hpp>
 #include <boost/mpl/pop_front.hpp>
@@ -38,8 +35,9 @@
 
 // boost::call_traits
 #include <boost/call_traits.hpp>
+
+// boost::function_types
 #include <boost/function_types/parameter_types.hpp>
-#include <boost/function_types/result_type.hpp>
 
 #include <mml/config.hpp>
 #include <mml/function_types/components.hpp>
@@ -51,14 +49,11 @@
 #   include <boost/fusion/include/vector.hpp>
 #   include <boost/fusion/include/at_c.hpp>
 #   include <mml/detail/replace_na_to_void.hpp>
-
-#   define _MML_OF_REPLACE_NA_TO_VOID(z, n, _)                              \
-        typename replace_na_to_void<f_type##n>::type                        \
-        /**/
+#   define _MML_IOF_GET_ITEM_FROM_SEQ(seq, n) (boost::fusion::at_c<n>(seq))
 #else
 #   include <mml/compressed_tuple/compressed_tuple.hpp>
+#   define _MML_IOF_GET_ITEM_FROM_SEQ(seq, n) (seq.template get<N>())
 #endif // MML_COMPRESSION_OFF
-
 
 #ifdef MML_RESULT_OF_SUPPORT_ON
 #   include <mml/result_of_support/result_of_support.hpp>
@@ -81,7 +76,7 @@ template <typename F>
     };
 
 
-#   define _MML_OF_CREATE_SIGNATURE(z, n, _)                                \
+#   define _MML_IOF_CREATE_SIGNATURE(z, n, _)                               \
         typename detail::create_signature<F##n>::type                       \
     /**/
 
@@ -94,11 +89,6 @@ namespace mml {
 
 namespace detail {
 
-#define _MML_OF_INVOKER_CALL_OPERATOR_FORMAL_PARAM(z, n, _)                 \
-    arg_type##n a##n                                                        \
-    /**/
-
-
     class root_invoker
     {
         struct dummy;
@@ -108,104 +98,102 @@ namespace detail {
 
 
     template <
-          typename Seq
+          typename Derived
+        , typename Targets
         , long N
         , long Arity =
             function_types::function_arity<
-                typename boost::mpl::at_c<Seq, N>::type
+                typename boost::mpl::at_c<Targets, N>::type
                 >::value
         >
-    class of_invoker;
+    class target_invoker;
 
 
-    // fwd
-    template <typename Seq>
-    class inline_overloaded_fn_over_seq;
-
-
-    template <typename Seq, long N>
+    template <
+          typename Derived
+        , typename Targets
+        , long N = boost::mpl::size<Targets>::value
+        >
     struct get_invoker_base
     {
-        typedef of_invoker<Seq, N - 1> type;
+        typedef target_invoker<Derived, Targets, N - 1> type;
     };
 
 
-    template <typename Seq>
-    struct get_invoker_base<Seq, 0>
+    template <typename Derived, typename Targets>
+    struct get_invoker_base<Derived, Targets, 0>
     {
         typedef root_invoker type;
     };
 
 
-#define _MML_OF_INVOKER_ARG_TYPEDEF(z, n, _)                                \
+#define _MML_TARGET_INVOKER_ARG_TYPEDEF(z, n, _)                            \
     typedef typename                                                        \
         boost::mpl::at_c<component_types, n + 1>::type arg_type##n;         \
     /**/
 
-#define _MML_OF_INVOKER_CALL_OPERATOR(n, const_kw)                          \
+#define _MML_TARGET_INVOKER_CALL_OPERATOR(n, const_kw)                      \
     r_type  operator()(                                                     \
-        BOOST_PP_ENUM(                                                      \
-                n                                                           \
-            , _MML_OF_INVOKER_CALL_OPERATOR_FORMAL_PARAM                    \
-            ,                                                               \
+        BOOST_PP_ENUM_BINARY_PARAMS(                                        \
+              n                                                             \
+            , arg_type                                                      \
+            , a                                                             \
             )                                                               \
         ) const_kw                                                          \
     {                                                                       \
         return                                                              \
-        static_cast<derived_class const_kw*>(this)->template get<N>()(      \
+        _MML_IOF_GET_ITEM_FROM_SEQ                                          \
+        (static_cast<Derived const_kw*>(this)->_targets, N)(                \
             BOOST_PP_ENUM_PARAMS(n, a)                                      \
             );                                                              \
     }                                                                       \
     /**/
 
 #if defined(_MSC_VER) && _MSC_VER < 1400
-#   define _MML_OF_INVOKER_CALL_OPERATOR_DEF(n)                             \
+#   define _MML_TARGET_INVOKER_CALL_OPERATOR_DEF(n)                         \
         /*workaround for old MSVC: only const operator() without nonconst*/ \
-        _MML_OF_INVOKER_CALL_OPERATOR(n, const)                             \
+        _MML_TARGET_INVOKER_CALL_OPERATOR(n, const)                         \
         /**/
 #else
-#   define _MML_OF_INVOKER_CALL_OPERATOR_DEF(n)                             \
-        _MML_OF_INVOKER_CALL_OPERATOR(n, )                                  \
-        _MML_OF_INVOKER_CALL_OPERATOR(n, const)                             \
+#   define _MML_TARGET_INVOKER_CALL_OPERATOR_DEF(n)                         \
+        _MML_TARGET_INVOKER_CALL_OPERATOR(n, )                              \
+        _MML_TARGET_INVOKER_CALL_OPERATOR(n, const)                         \
         /**/
 #endif // defined(_MSC_VER) && _MSC_VER < 1400
 
-#define _MML_OF_INVOKER(z, n, _)                                            \
-    template <typename Seq, long N>                                         \
-    class of_invoker<Seq, N, n>                                             \
-        : public get_invoker_base<Seq, N>::type                             \
+#define _MML_TARGET_INVOKER(z, n, _)                                        \
+    template <typename Derived, typename Targets, long N>                   \
+    class target_invoker<Derived, Targets, N, n>                            \
+        : public get_invoker_base<Derived, Targets, N>::type                \
     {                                                                       \
         typedef typename                                                    \
-            get_invoker_base<Seq, N>::type base_class;                      \
-                                                                            \
-        typedef                                                             \
-            inline_overloaded_fn_over_seq<Seq> derived_class;               \
+            get_invoker_base<Derived, Targets, N>::type base_class;         \
                                                                             \
         typedef typename                                                    \
-            boost::mpl::at_c<Seq, N>::type f_type;                          \
+            boost::mpl::at_c<Targets, N>::type target_type;                 \
                                                                             \
         typedef typename                                                    \
-            function_types::components<f_type>::types component_types;      \
+            function_types::components<target_type>::types component_types; \
                                                                             \
         typedef typename                                                    \
             boost::mpl::front<component_types>::type r_type;                \
                                                                             \
         BOOST_PP_REPEAT(                                                    \
               n                                                             \
-            , _MML_OF_INVOKER_ARG_TYPEDEF                                   \
+            , _MML_TARGET_INVOKER_ARG_TYPEDEF                               \
             ,                                                               \
             )                                                               \
                                                                             \
     public:                                                                 \
         using base_class::operator();                                       \
-        _MML_OF_INVOKER_CALL_OPERATOR_DEF(n)                                \
+        _MML_TARGET_INVOKER_CALL_OPERATOR_DEF(n)                            \
     };                                                                      \
     /**/
 
 
     BOOST_PP_REPEAT(
           BOOST_PP_INC(MML_MAX_ARITY)
-        , _MML_OF_INVOKER
+        , _MML_TARGET_INVOKER
         ,
         )
 
@@ -242,153 +230,46 @@ namespace detail {
     };
 
 
-    template <typename Seq, long N>
-    struct safe_at_c
-        : boost::mpl::eval_if_c<
-              N < boost::mpl::size<Seq>::value
-            , boost::mpl::at_c<Seq, N>
-            , boost::mpl::na
-            >
-    {
-    };
-
-
-#define _MML_OF_TEMPLATE_FACT_PARAM_LIST                                    \
+#define _MML_IOF_TEMPLATE_FACT_PARAM_LIST                                   \
     BOOST_PP_ENUM_PARAMS(MML_MAX_FUNCTION_OVERLOADS, F)                     \
     /**/
 
-#define _MML_OF_F_TYPEDEF(z, n, _)                                          \
-    typedef typename safe_at_c<Seq, n>::type f_type##n;                     \
-    /**/
 
-#define _MML_OF_CONSTRUCTOR_FORMAL_PARAM(z, n, f_t)                         \
-    typename boost::call_traits<f_t##n>::param_type f##n                    \
-    /**/
-
-#define _MML_OF_SEQ_CONSTRUCTOR(z, n, _)                                    \
-    explicit inline_overloaded_fn_over_seq(                                 \
-        BOOST_PP_ENUM(                                                      \
-              n                                                             \
-            , _MML_OF_CONSTRUCTOR_FORMAL_PARAM                              \
-            , f_type                                                        \
-            )                                                               \
-        )                                                                   \
-        : _funcs(BOOST_PP_ENUM_PARAMS(n, f))                                \
-    {                                                                       \
-    }                                                                       \
-    /**/
-
-
-    template <typename Seq>
-    class inline_overloaded_fn_over_seq
-        :
-#ifdef __GNUC__
-        protected   // GCC workaround of inaccessible base
-#else
-        private     // correct case
-#endif
-        get_invoker_base<
-              Seq
-            , boost::mpl::size<Seq>::value
+    template <
+          typename Derived
+        , BOOST_PP_ENUM_PARAMS(MML_MAX_FUNCTION_OVERLOADS, typename F)
+        >
+    class inline_overloaded_fn_base
+        : protected get_invoker_base<
+              Derived
+              , boost::mpl::vector< _MML_IOF_TEMPLATE_FACT_PARAM_LIST >
             >::type
     {
-        typedef typename get_invoker_base<
-              Seq
-            , boost::mpl::size<Seq>::value
-            >::type base_class;
-
-        BOOST_PP_REPEAT(
-              MML_MAX_FUNCTION_OVERLOADS
-            , _MML_OF_F_TYPEDEF
-            ,
-            )
-
+    protected:
         typedef
 #ifdef MML_COMPRESSION_OFF
+#   define _MML_IOF_REPLACE_NA_TO_VOID(z, n, _)                             \
+        typename replace_na_to_void<F##n>::type                             \
+        /**/
+
         boost::fusion::vector<
             BOOST_PP_ENUM(
                   MML_MAX_FUNCTION_OVERLOADS
-                , _MML_OF_REPLACE_NA_TO_VOID
+                , _MML_IOF_REPLACE_NA_TO_VOID
                 ,
                 )
             >
-#   undef _MML_OF_REPLACE_NA_TO_VOID
+#   undef _MML_IOF_REPLACE_NA_TO_VOID
 #else
-        compressed_tuple<
-            BOOST_PP_ENUM_PARAMS(
-                  MML_MAX_FUNCTION_OVERLOADS
-                , f_type
-                )
-            >
+        compressed_tuple<_MML_IOF_TEMPLATE_FACT_PARAM_LIST>
 #endif // MML_COMPRESSION_OFF
         tuple_type;
-
-    public:
-        using base_class::operator();
 
         typedef typename tuple_type::types types;
         typedef typename boost::mpl::transform<
               types
             , function_types::function_type<boost::mpl::_>
             >::type signatures;
-
-        template <long N>
-        typename boost::call_traits<
-            typename boost::mpl::at_c<types, N>::type
-            >::reference
-        get()
-        {
-            return
-#ifdef MML_COMPRESSION_OFF
-            boost::fusion::at_c<N>(_funcs);
-#else
-            _funcs.template get<N>();
-#endif // MML_COMPRESSION_OFF
-        }
-
-        template <long N>
-        typename boost::call_traits<
-            typename boost::mpl::at_c<types, N>::type
-            >::const_reference
-        get() const
-        {
-            return const_cast<inline_overloaded_fn_over_seq*>(this)
-                ->template get<N>();
-        }
-
-        template <typename F>
-        typename boost::call_traits<F>::reference
-        get()
-        {
-            namespace _bmpl = boost::mpl;
-            return get<
-                _bmpl::distance<
-                      typename _bmpl::begin<types>::type
-                    , typename _bmpl::find<
-                        types, F
-                        >::type
-                    >::value
-                >();
-        }
-
-        template <typename F>
-        typename boost::call_traits<F>::const_reference
-        get() const
-        {
-            return const_cast<inline_overloaded_fn_over_seq*>(this)
-                ->template get<F>();
-        }
-
-    protected:
-        BOOST_PP_REPEAT(
-              BOOST_PP_INC(MML_MAX_FUNCTION_OVERLOADS)
-            , _MML_OF_SEQ_CONSTRUCTOR
-            ,
-            )
-
-        ~inline_overloaded_fn_over_seq()
-        {
-        }
 
     private:
         BOOST_MPL_ASSERT_MSG(
@@ -399,24 +280,26 @@ namespace detail {
                     >::type
                 >::value)
             , TWO_OR_MORE_OVERLOADED_FUNCTIONS_HAVE_EQUAL_PARAMETER_LISTS
-            , (typename inline_overloaded_fn_over_seq::signatures)
+            , (typename inline_overloaded_fn_base::signatures)
             );
-
-        tuple_type _funcs;
     };
 
 } // namespace detail
 
 
-#define _MML_OF_CONSTRUCTOR(z, n, _)                                        \
+#define _MML_IOF_CONSTRUCTOR_FORMAL_PARAM(z, n, _)                          \
+    typename boost::call_traits<F##n>::param_type f##n                      \
+    /**/
+
+#define _MML_IOF_CONSTRUCTOR(z, n, _)                                       \
     explicit inline_overloaded_fn(                                          \
         BOOST_PP_ENUM(                                                      \
               n                                                             \
-            , _MML_OF_CONSTRUCTOR_FORMAL_PARAM                              \
-            , F                                                             \
+            , _MML_IOF_CONSTRUCTOR_FORMAL_PARAM                             \
+            ,                                                               \
             )                                                               \
         )                                                                   \
-        : base_class(BOOST_PP_ENUM_PARAMS(n, f))                            \
+        : _targets(BOOST_PP_ENUM_PARAMS(n, f))                              \
     {                                                                       \
     }                                                                       \
     /**/
@@ -430,42 +313,94 @@ template <
         )
     >
 class inline_overloaded_fn
-    : public detail::inline_overloaded_fn_over_seq<
-        boost::mpl::vector<_MML_OF_TEMPLATE_FACT_PARAM_LIST>
+    : private detail::inline_overloaded_fn_base<
+          inline_overloaded_fn<_MML_IOF_TEMPLATE_FACT_PARAM_LIST>
+        , _MML_IOF_TEMPLATE_FACT_PARAM_LIST
         >
 #ifdef MML_RESULT_OF_SUPPORT_ON
     , public mml::result_of_support<
-        BOOST_PP_ENUM(MML_MAX_FUNCTION_OVERLOADS, _MML_OF_CREATE_SIGNATURE, )
+        BOOST_PP_ENUM(MML_MAX_FUNCTION_OVERLOADS, _MML_IOF_CREATE_SIGNATURE, )
         >
-#   undef _MML_OF_CREATE_SIGNATURE
+#   undef _MML_IOF_CREATE_SIGNATURE
 #endif // MML_RESULT_OF_SUPPORT_ON
 {
-    typedef detail::inline_overloaded_fn_over_seq<
-        boost::mpl::vector<_MML_OF_TEMPLATE_FACT_PARAM_LIST>
+    template <typename Derived, typename Targets, long N, long Arity>
+    friend class detail::target_invoker;
+
+    typedef detail::inline_overloaded_fn_base<
+          inline_overloaded_fn<_MML_IOF_TEMPLATE_FACT_PARAM_LIST>
+        , _MML_IOF_TEMPLATE_FACT_PARAM_LIST
         > base_class;
 
 public:
+    typedef typename base_class::types types;
+    using base_class::signatures;
+    using base_class::operator();
+
     BOOST_PP_REPEAT(
           BOOST_PP_INC(MML_MAX_FUNCTION_OVERLOADS)
-        , _MML_OF_CONSTRUCTOR
+        , _MML_IOF_CONSTRUCTOR
         ,
         )
+
+    template <long N>
+    typename boost::call_traits<
+        typename boost::mpl::at_c<types, N>::type
+        >::reference
+    target()
+    {
+        return _MML_IOF_GET_ITEM_FROM_SEQ(_targets, N);
+    }
+
+    template <long N>
+    typename boost::call_traits<
+        typename boost::mpl::at_c<types, N>::type
+        >::const_reference
+    target() const
+    {
+        return const_cast<inline_overloaded_fn*>(this)
+            ->template target<N>();
+    }
+
+    template <typename F>
+    typename boost::call_traits<F>::reference
+    target()
+    {
+        namespace _bmpl = boost::mpl;
+        return target<
+            _bmpl::distance<
+                  typename _bmpl::begin<types>::type
+                , typename _bmpl::find<
+                    types, F
+                    >::type
+                >::value
+            >();
+    }
+
+    template <typename F>
+    typename boost::call_traits<F>::const_reference
+    target() const
+    {
+        return const_cast<inline_overloaded_fn*>(this)
+            ->template target<F>();
+    }
+
+private:
+    typename base_class::tuple_type _targets;
 };
 
 } // namespace mml
 
 
 // undef temporaries
-#undef _MML_OF_INVOKER_CALL_OPERATOR_FORMAL_PARAM
-#undef _MML_OF_INVOKER_ARG_TYPEDEF
-#undef _MML_OF_INVOKER_CALL_OPERATOR
-#undef _MML_OF_INVOKER_CALL_OPERATOR_DEF
-#undef _MML_OF_INVOKER
-#undef _MML_OF_TEMPLATE_FACT_PARAM_LIST
-#undef _MML_OF_F_TYPEDEF
-#undef _MML_OF_CONSTRUCTOR_FORMAL_PARAM
-#undef _MML_OF_SEQ_CONSTRUCTOR
-#undef _MML_OF_CONSTRUCTOR
+#undef _MML_IOF_GET_ITEM_FROM_SEQ
+#undef _MML_TARGET_INVOKER_ARG_TYPEDEF
+#undef _MML_TARGET_INVOKER_CALL_OPERATOR
+#undef _MML_TARGET_INVOKER_CALL_OPERATOR_DEF
+#undef _MML_TARGET_INVOKER
+#undef _MML_IOF_TEMPLATE_FACT_PARAM_LIST
+#undef _MML_IOF_CONSTRUCTOR_FORMAL_PARAM
+#undef _MML_IOF_CONSTRUCTOR
 
 
 #endif // _MML_INLINE_OVERLOADED_FN_INLINE_OVERLOADED_FN_HPP_INCLUDED_
